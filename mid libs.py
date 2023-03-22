9cc7508ce5aca0afebe2308e1ac00219bf92fe0b
ahmedsamymohamad <116212699+ahmedsamymohamad@users.noreply.github.com>
2023-03-23 00:50:34 +0200

    Add files via upload

---
+++ mid libs.py
@@ -0,0 +1,29 @@
+
+# Mad libs Python Project
+
+print("Welcome to Mad libs!")
+print("Please fill in the blanks below:")
+
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")
+bird = input("Enter a type of bird: ")
+room = input("Enter a room in a house: ")
+verb1 = input("Enter a verb (past tense): ")
+verb2 = input("Enter a verb: ")
+name = input("Enter a relative's name: ")
+noun1 = input("Enter a noun: ")
+liquid = input("Enter a liquid: ")
+verb3 = input("Enter a verb ending in -ing: ")
+body_part = input("Enter a body part (plural): ")
+noun2 = input("Enter a plural noun: ")
+verb4 = input("Enter a verb ending in -ing: ")
+noun3 = input("Enter a noun: ")
+
+# Generate the Mad libs story
+print(f"It was a {adjective1}, cold November day. I woke up to the {adjective2} smell of {bird} roasting in the {room} downstairs. I {verb1} down the stairs to see if I could help {verb2} the dinner. My mom said, 'See if {name} needs a fresh {noun1}.' So I carried a tray of glasses full of {liquid} into the {verb3} room. When I got there, I couldn't believe my {body_part}! There were {noun2} {verb4} on the {noun3}!")
+
+
+#When you run this program, it will prompt the user to enter various words (adjectives, nouns, verbs, etc.), and then it will generate a Mad libs story using those words. Here's an example     
+
+
+#It was a rainy, cold November day. I woke up to the smelly smell of turkey roasting in the kitchen downstairs. I danced down the stairs to see if I could help cook the dinner. My mom said, 'See if Uncle Bob needs a fresh fork.' So I carried a tray of glasses full of gravy into the singing room. When I got there, I couldn't believe my feet! There were potatoes peeling on the table!
